0d31a2c6f50dcbfe47143cb49b16b85a14362ef5
Ismoil98 <ismoil6366>
2024-01-12 08:33:12 +0500

    added

---
+++ main.js
@@ -0,0 +1,125 @@
+// Array1
+// let Level2 = []
+// function getLevel2(n) {
+//     for (let i=0; i<n; i++){
+//         Level2[i]=Math.pow(2,i+1)
+//     }
+//     return Level2
+// }
+// let i = 5
+// let x = getLevel2(i)
+// console.log(x)
+
+
+
+// Array2
+// let n = 5
+// let A = 2
+// let B = 3
+// let Arr = []
+// Arr[0] = A
+// Arr[1] = B
+// let S=A+B
+// for (let i=2; i<n; i++){
+//     Arr[i] = S
+//     S += Arr[i]
+// }
+// console.log(Arr);
+
+
+
+// Array3
+// let n = +prompt('n=')
+// let Arr = []
+// for (let i=0; i<n; i++) {
+//     Arr[i]=+prompt(`${i} element:`)
+// }
+// console.log(Arr);
+// console.log(Arr.reverse());
+
+
+
+// Array4
+// let Arr = [4, 9, 7, 8, 6, 5]
+// let s = 0
+// for (let i=0; i<Arr.length; i++) {
+//     // console.log(i);
+//     if (Arr[i] % 2 == 1) {
+//         console.log(Arr[i]);
+//         s++
+//     }
+// }
+// console.log(`Toq elementlar soni ${s}`);
+
+
+
+// Array5
+
+
+
+// Array6
+// let n = +prompt('n=')
+// let Arr = []
+// for (let i=0; i<n; i++) {
+//     Arr[i] = +prompt()
+// }
+// console.log(Arr);
+// for (let i=0; i<=n; i+=2) {
+//     console.log(Arr[i]);
+// }
+
+
+
+// Array7
+// let n = +prompt('n=')
+// let Arr = []
+// for (let i=0; i<n; i++) {
+//     Arr[i] = +prompt()
+// }
+// console.log(Arr);
+// let newArr = Arr.reverse()
+// for (let i=0; i<=n; i+=2) {
+//     console.log(newArr[i]);
+// }
+
+
+
+
+// Array8
+// let n = +prompt('n=')
+// let Arr = []
+// for (let i=0; i<n; i++) {
+//     Arr[i] = +prompt()
+// }
+// console.log(Arr);
+// for (let i=0; i<=n; i+=2) {
+//     console.log(Arr[i]);
+// }
+// for (let i=1; i<=n; i+=2) {
+//     console.log(Arr[i]);
+// }
+
+
+// Array9
+// let n = +prompt('n=')
+// let Arr = []
+// for (let i=0; i<n; i++) {
+//     Arr[i] = +prompt()
+// }
+// console.log(Arr);
+// for (let i=0; i<=n; i+=2) {
+//     console.log(Arr[i]);
+// }
+// let newArr = Arr.reverse()
+// for (let i=1; i<=n; i+=2) {
+//     console.log(newArr[i]);
+// }
+
+
+
+// Array10
+
+
+
+// Array11
+w
